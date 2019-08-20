047cd4db0b948f5dfcb25bf49c4792b136082bf7
kirill <kirill@Mac-mini-kirill.local>
2019-08-20 11:54:37 +0300

    part5 -> 12.mp4

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/auth.routes')
 const postRoutes = require('./routes/post.routes')
+const commentRoutes = require('./routes/comment.routes')
 const passport = require('passport')
 
 const passportStrategy = require('./middleware/passport-strategy')
@@ -21,5 +22,6 @@ app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
 
 module.exports = app
