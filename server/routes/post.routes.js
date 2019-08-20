2e7878e0e6c6a61cce6f859b7f0dd6a958eb33d8
kirill <kirill@Mac-mini-kirill.local>
2019-08-20 10:49:50 +0300

    part5 -> 10.mp4

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const {Router} = require('express')
+const upload = require('../middleware/upload')
 const ctr = require('../controllers/post.controller')
 const router = Router()
 
@@ -7,6 +8,7 @@ const router = Router()
 //- /api/post/admin
 router.post(
   '/admin/',
+  upload.single('image'), 
   passport.authenticate('jwt', {session: false}),
   ctr.create
 )
