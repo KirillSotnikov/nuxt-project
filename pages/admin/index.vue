71ea0849c467e4a5ed5fd73e058504074155be1d
kirill <kirill@Mac-mini-kirill.local>
2019-08-21 12:44:44 +0300

    part7 -> 3.mp4

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,57 +1,30 @@
 <template>
   <div>
     <h1>Posts Analytic</h1>
-    <canvas height="80vh" ref="canvas"></canvas>
+    <app-analytics-chart 
+      title="Quantity of views"
+      :labels="views.labels"
+      :data="views.data"
+    ></app-analytics-chart>
+    <app-analytics-chart 
+      title="Quantity of comments"
+      :labels="comments.labels"
+      :data="comments.data"
+    ></app-analytics-chart>
   </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
+import AppAnalyticsChart from '@/components/admin/AnalyticChart'
 export default {
   layout: 'admin',
   middleware: ['admin-auth'],
-  extends: Bar,
-  async asyncData({store}) {
-    const analytics = await store.dispatch('post/getAnalytics')
-    return {analytics}
+  components: {
+    AppAnalyticsChart
   },
-  mounted() {
-    console.log(this.analytics)
-    
-    const data =  {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-    const options = {
-      scales: {
-        yAxes: [{
-          ticks: {
-              beginAtZero: true
-          }
-        }]
-      }
-    }
-    this.renderChart(data, options)
+  async asyncData({store}) {
+    const {views, comments} = await store.dispatch('post/getAnalytics')
+    return {views, comments} 
   }
 }
 </script>
